24620dd553b11235a40ce8d6e663c1e42d501d70
vikramsaluja <vikram.saluja@menloschool.com>
2024-11-19 20:15:13 -0800

    Finished Deck and Player class

--- src/Deck.java
+++ src/Deck.java
@@ -4,16 +4,19 @@ public class Deck {
     private ArrayList<Card> cards;
     private int cardsLeft;
 
+    // Deck Constructor
     public Deck(String[] ranks, String[] suits, int[] values) {
         cards = new ArrayList<Card>();
         for (String suit : suits){
             for(int i = 0; i < ranks.length; i++){
+                // creates new card and sets variables
                  cards.add(new Card(ranks[i], suit, values[i]));
             }
         }
         cardsLeft = cards.size();
     }
 
+    // Returns true if there are no cards left in deck
     public boolean isEmpty(){
         return cardsLeft == 0;
     }
@@ -23,11 +26,28 @@ public class Deck {
     }
 
     public Card deal(){
+        // Returning null if there arent cards left
         if(isEmpty()){
             return null;
         }
         cardsLeft-- ;
         return cards.get(cardsLeft);
+    }
+
+    public void shuffle(){
+        // For loop that runs starting at the highest index to the lowest
+        for(int i = cards.size() -1; i > 0; i++){
+            // Generates a random integer in between 0 and i
+            int random = (int) (Math.random() * (i + 1));
+
+            // Swaps card at i a card at a random index
+            Card swap = cards.get(i);
+            cards.set(i, cards.get(random));
+            cards.set(random, swap);
+
+        }
+        // Resets the number of cards left in the deck
+        cardsLeft = cards.size();
     }
 
 }
---
+++ src/Player.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private ArrayList<Card> hand;
+    private int points;
+
+    // Constructor takes in player name and sets points to 0
+    public Player(String name){
+        this.name = name;
+        this.points = 0;
+    }
+
+    // Constructor takes in player name, hand of cards, sets points to 0
+    public Player(String name, ArrayList<Card> newHand){
+        this.name = name;
+        this.hand = newHand;
+        this.points = points;
+    }
+
+    // Getter and setter methods for instance variables
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setCards(ArrayList<Card> cards) {
+        this.hand= cards;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    // Takes in number of points and adds it to player's points
+    public void addPoints(int points){
+        this.points += points;
+    }
+
+    // Adds new card to the players hand
+    public void addCard(Card newCard){
+        hand.add(newCard);
+    }
+
+    @Override
+    public String toString() {
+        return (this.name + " has " + this.points + " points\n" + this.name + "'s cards " + this.hand);
+    }
+}
